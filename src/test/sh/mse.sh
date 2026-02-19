99d59f24854821c0c85bedb8133d9e4d5ef858f8
Pierre-Anthony Lemieux <pal@sandflow.com>
2026-02-18 22:58:30 -0800

    Improve stat script

--- src/test/sh/mse.sh
+++ src/test/sh/mse.sh
@@ -5,37 +5,70 @@ set -e
 SRC="${1:-SPARKS_ACES_07500.exr}"
 FN=$(basename "${SRC}")
 
-# generate DWA results
-DWA_Q="45"
+run_dwa() {
+  # generate DWA results
+  DWA_Q="20 40 60 80 100"
 
-echo "DWA"
-echo "Q, MSE, SIZE"
+  echo "DWA"
+  echo "Q, MSE, SIZE"
 
-for q in $DWA_Q; do
-  DWA_FN="${FN%.exr}.dwa.${q}.exr"
-  ./bin/exrmetrics ${SRC} -z dwab --convert -o ${DWA_FN} -l ${q}
-  SIZE=$(stat -c%s -- "$DWA_FN")
-  MSE=$(./bin/exrmse ${SRC} ${DWA_FN})
-  echo "${q},${MSE},${SIZE}"
-done
+  for q in $DWA_Q; do
+    DWA_FN="${FN%.exr}.dwa.${q}.exr"
+    ./bin/exrmetrics ${SRC} -z dwab --convert -o ${DWA_FN} -l ${q}
+    SIZE=$(stat -c%s -- "$DWA_FN")
+    MSE=$(./bin/exrmse -n ${SRC} ${DWA_FN})
+    echo "${q},${MSE},${SIZE}"
+  done
+}
+#run_dwa
 
-# generate HTL results
+run_ojph() {
+  # generate ojph results
 
-HT_Q="0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0007 0.0008 0.0009 0.001"
+  HT_Q="0.00005 0.0001 0.0002 0.0004 0.0008"
 
-echo "HT"
-echo "Q, MSE, SIZE"
+  echo "OJPH"
+  echo "Q, MSE, SIZE"
 
-for q in $HT_Q; do
-  HT_FN="${FN%.exr}.ht.${q}.exr"
-  PIZ_FN="${FN%.exr}.ht.${q}.piz.exr"
-  ./bin/exrj2klossy_enc ${SRC} ${HT_FN} -q ${q} > /dev/null
-  ./bin/exrmetrics ${HT_FN} -z piz --convert -o ${PIZ_FN}
-  SIZE=$(stat -c%s -- "$HT_FN")
-  MSE=$(./bin/exrmse ${SRC} ${HT_FN})
-  rm ${HT_FN}
-  echo "${q},${MSE},${SIZE}"
-done
+  for q in $HT_Q; do
+    HT_FN="${FN%.exr}.ht.${q}.exr"
+    PIZ_FN="${FN%.exr}.ht.${q}.piz.exr"
+    HTL_FN="${FN%.exr}.ht.${q}.htl.exr"
+    ./bin/exrj2klossy_enc ${SRC} ${HT_FN} -q ${q} -t > /dev/null
+    SIZE=$(stat -c%s -- "$HT_FN")
+    ./bin/exrj2klossy_dec -t ${HT_FN} $HTL_FN > /dev/null
+    ./bin/exrmetrics ${HTL_FN} -z piz --convert -o ${PIZ_FN}
+    MSE=$(./bin/exrmse -n  ${SRC} ${PIZ_FN})
+    rm ${HT_FN}
+    rm ${HTL_FN}
+    echo "${q},${MSE},${SIZE}"
+  done
+}
+#run_ojph
+
+run_kdu() {
+  # generate KDU results
+
+  HT_R="6 8 10 14 20"
+
+  echo "KDU"
+  echo "R, MSE, SIZE"
+
+  for r in $HT_R; do
+    HT_FN="${FN%.exr}.kdu.${r}.exr"
+    PIZ_FN="${FN%.exr}.kdu.${r}.piz.exr"
+    HTL_FN="${FN%.exr}.kdu.${r}.htl.exr"
+    ./bin/exrj2klossy_enc ${SRC} ${HT_FN} -r ${r} -t > /dev/null
+    SIZE=$(stat -c%s -- "$HT_FN")
+    ./bin/exrj2klossy_dec -t ${HT_FN} $HTL_FN > /dev/null
+    ./bin/exrmetrics ${HTL_FN} -z piz --convert -o ${PIZ_FN}
+    MSE=$(./bin/exrmse -n  ${SRC} ${PIZ_FN})
+    rm ${HT_FN}
+    rm ${HTL_FN}
+    echo "${r},${MSE},${SIZE}"
+  done
+}
+run_kdu
 
 # ./bin/exrmetrics SPARKS_ACES_01000.exr -z dwab --convert -o SPARKS_ACES_01000.dwaa.exr
 # ./bin/exrmse SPARKS_ACES_01000.exr SPARKS_ACES_01000.htl.exr
