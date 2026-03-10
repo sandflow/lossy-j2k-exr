88272bd5d96a405f613c75596fbf9092b08ca756
Pierre-Anthony Lemieux <pal@sandflow.com>
2026-03-10 16:39:35 -0700

    Added linear MSE

--- src/test/sh/mse.sh
+++ src/test/sh/mse.sh
@@ -7,17 +7,18 @@ FN=$(basename "${SRC}")
 
 run_dwa() {
   # generate DWA results
-  DWA_Q="20 40 60 80 100"
+  DWA_Q="20 40 60 80 100 160"
 
   echo "DWA"
-  echo "Q, MSE, SIZE"
+  echo "Q, MSE, MSE (linear), SIZE"
 
   for q in $DWA_Q; do
     DWA_FN="${FN%.exr}.dwa.${q}.exr"
     ./bin/exrmetrics ${SRC} -z dwab --convert -o ${DWA_FN} -l ${q}
     SIZE=$(stat -c%s -- "$DWA_FN")
     MSE=$(./bin/exrmse -n ${SRC} ${DWA_FN})
-    echo "${q},${MSE},${SIZE}"
+    MSELIN=$(./bin/exrmse ${SRC} ${DWA_FN})
+    echo "${q},${MSE},${MSELIN},${SIZE}"
   done
 }
 run_dwa
@@ -46,6 +47,30 @@ run_ojph() {
 }
 run_ojph
 
+run_ojph_linear() {
+  # generate ojph results
+
+  HT_Q="0.00005 0.0001 0.0002 0.0004 0.0008"
+
+  echo "OJPH (linear)"
+  echo "Q, MSE, SIZE"
+
+  for q in $HT_Q; do
+    HT_FN="${FN%.exr}.ht.${q}.exr"
+    PIZ_FN="${FN%.exr}.ht.${q}.piz.exr"
+    HTL_FN="${FN%.exr}.ht.${q}.htl.exr"
+    ./bin/exrj2klossy_enc ${SRC} ${HT_FN} -q ${q} > /dev/null
+    SIZE=$(stat -c%s -- "$HT_FN")
+    ./bin/exrj2klossy_dec ${HT_FN} $HTL_FN > /dev/null
+    ./bin/exrmetrics ${HTL_FN} -z piz --convert -o ${PIZ_FN}
+    MSE=$(./bin/exrmse ${SRC} ${PIZ_FN})
+    rm ${HT_FN}
+    rm ${HTL_FN}
+    echo "${q},${MSE},${SIZE}"
+  done
+}
+run_ojph_linear
+
 run_kdu() {
   # generate KDU results
 
@@ -69,6 +94,30 @@ run_kdu() {
   done
 }
 run_kdu
+
+run_kdu_linear() {
+  # generate KDU results
+
+  HT_R="6 8 10 14 20"
+
+  echo "KDU linear"
+  echo "R, MSE, SIZE"
+
+  for r in $HT_R; do
+    HT_FN="${FN%.exr}.kdu.${r}.exr"
+    PIZ_FN="${FN%.exr}.kdu.linear.${r}.piz.exr"
+    HTL_FN="${FN%.exr}.kdu.${r}.htl.exr"
+    ./bin/exrj2klossy_enc ${SRC} ${HT_FN} -r ${r} > /dev/null
+    SIZE=$(stat -c%s -- "$HT_FN")
+    ./bin/exrj2klossy_dec ${HT_FN} $HTL_FN > /dev/null
+    ./bin/exrmetrics ${HTL_FN} -z piz --convert -o ${PIZ_FN}
+    MSE=$(./bin/exrmse ${SRC} ${PIZ_FN})
+    rm ${HT_FN}
+    rm ${HTL_FN}
+    echo "${r},${MSE},${SIZE}"
+  done
+}
+run_kdu_linear
 
 # ./bin/exrmetrics SPARKS_ACES_01000.exr -z dwab --convert -o SPARKS_ACES_01000.dwaa.exr
 # ./bin/exrmse SPARKS_ACES_01000.exr SPARKS_ACES_01000.htl.exr
